e144b079266d1f151e47bccaa7848ebdd2f982f9
Paul Sonnenschein <paulsonnenschein0@gmail.com>
2018-04-17 08:27:52 +0200

    Provide const array of providers (but stay backwards compatible)

--- lib/hammock.dart
+++ lib/hammock.dart
@@ -15,31 +15,31 @@ part 'src/object_store.dart';
 part 'src/utils.dart';
 part 'src/default_header.dart';
 
+const hammockProviders = const [
+  const Provider(HttpDefaultHeaders, useFactory: getHttpDefaultHeaders, deps: const []),
+  const Provider(ResourceStore, useFactory: getResourceStore, deps: const [Client, HammockConfig, HttpDefaultHeaders]),
+  const Provider(ObjectStore, useFactory: getObjectStore, deps: const [ResourceStore, HammockConfig]),
+  const Provider(Client, useFactory: getBrowserClient)
+];
+
+BrowserClient getBrowserClient() {
+  return new BrowserClient()..withCredentials = true;
+}
+
+ResourceStore getResourceStore(Client client, HammockConfig config, HttpDefaultHeaders h) {
+  return new ResourceStore(client, config, h);
+}
+
+ObjectStore getObjectStore(ResourceStore resourceStore, HammockConfig config) {
+  return new ObjectStore(resourceStore, config);
+}
+
+HttpDefaultHeaders getHttpDefaultHeaders() {
+  return new HttpDefaultHeaders();
+}
+
 class Hammock {
-
   static getProviders() {
-    return [
-      //provide(HammockConfig),
-      provide(HttpDefaultHeaders, useFactory: getHttpDefaultHeaders, deps: const []),
-      provide(ResourceStore, useFactory: getResourceStore, deps: const [Client, HammockConfig, HttpDefaultHeaders]),
-      provide(ObjectStore, useFactory: getObjectStore, deps: const [ResourceStore, HammockConfig]),
-      provide(Client, useFactory: getBrowserClient),
-    ];
-  }
-
-  static getBrowserClient() {
-    return new BrowserClient()..withCredentials = true;
-  }
-
-  static getResourceStore(Client client, HammockConfig config, HttpDefaultHeaders h) {
-    return new ResourceStore(client, config, h);
-  }
-
-  static getObjectStore(ResourceStore resourceStore, HammockConfig config) {
-    return new ObjectStore(resourceStore, config);
-  }
-
-  static getHttpDefaultHeaders() {
-    return new HttpDefaultHeaders();
+    return hammockProviders;
   }
 }
